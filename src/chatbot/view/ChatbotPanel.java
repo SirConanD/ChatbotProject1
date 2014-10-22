d906dd49d61c10ccb34289bfb378b87a9dce896b
SirConanD <fetzegra000@my.jordandistrict.org>
2014-10-22 09:51:37 -0600

    Added code for a scroll pane

--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,11 +1,10 @@
 package chatbot.view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SpringLayout;
+import javax.swing.*;
 
 import chatbot.controller.ChatbotAppController;
 
@@ -20,6 +19,8 @@ public class ChatbotPanel extends JPanel
 	 * Creates swing text field.
 	 */
 	private JTextField sampleField;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
 	/**
 	 * creates a layout for the panel.
 	 */
@@ -33,8 +34,14 @@ public class ChatbotPanel extends JPanel
 		this.baseController = baseController;
 		sampleButton = new JButton("click on me please.");
 		sampleField = new JTextField(25);
+		chatArea=new JTextArea(5, 25);
+		chatPane = new JScrollPane(chatArea);
 		
 		baseLayout = new SpringLayout();
+		baseLayout.putConstraint(SpringLayout.SOUTH, sampleButton, -19, SpringLayout.NORTH, chatPane);
+		baseLayout.putConstraint(SpringLayout.EAST, sampleButton, -140, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatPane, 150, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 100, SpringLayout.NORTH, this);
 		
 		
 		setupPanel();
@@ -44,6 +51,13 @@ public class ChatbotPanel extends JPanel
 	/**
 	 * Sets up what goes in the frame.
 	 */
+	
+	private void setupScrollPane()
+	{
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+	}
+	
 	private void setupPanel()
 	{
 		this.setBackground(Color.ORANGE);
@@ -51,23 +65,29 @@ public class ChatbotPanel extends JPanel
 		
 		this.add(sampleButton);
 		this.add(sampleField);
+		this.add(chatPane);
 	}
 	/**
 	 * Where all auto generated code goes.
 	 */
 	private void setupLayout()
 	{
-		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 178, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 129, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, sampleField, -32, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, sampleField, -66, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, sampleButton, 10, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.EAST, sampleButton, -10, SpringLayout.EAST, this);
 	}
     /**
      * Creates a listener for buttons.
      */
 	private void setupListeners()
 	{
+		sampleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				sampleField.setText(sampleField.getText() +":O omg");
+			}
+		});
 		
 	}
 	
