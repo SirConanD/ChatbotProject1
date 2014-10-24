b984952ca7973a63b20b2139e2bf9e82326af1d2
SirConanD <fetzegra000@my.jordandistrict.org>
2014-10-24 09:46:46 -0600

    Made it so a message will not show.

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -57,8 +57,8 @@ public class ChatbotAppController
 	 */
 	public void start()
 	{
-		String message = appView.displayChatbotConversations(startMessage);
-		
+//		String message = appView.displayChatbotConversations(startMessage);
+//		
 //		while(!notSoCleverBot.quitChecker(message))
 //		{
 //			message = notSoCleverBot.processText(message);
